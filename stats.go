300ac4df38c7d870af1c39ab78f17e4e34ba3dff
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-02-25 09:25:16 +0100

    Use `du` to get directory size

--- stats.go
+++ stats.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"errors"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
-	"syscall"
+	"strings"
 	"time"
+	"unicode"
 )
 
 type stats struct {
@@ -34,17 +35,17 @@ func newStats(stores []KV) *stats {
 }
 
 func (s *stats) measure(kv, operation, path string, n int, fn func()) {
-	startSize, _ := getSize(path)
+	startSize, _ := du(path)
 	startTime := time.Now().UnixNano()
 	fn()
 	endTime := time.Now().UnixNano()
-	endSize, _ := getSize(path)
+	endSize, _ := du(path)
 
 	w := s.w[kv]
 	w.Write([]string{operation, strconv.Itoa(n),
 		strconv.FormatInt(endTime-startTime, 10),
-		strconv.FormatFloat(float64(startSize)/1024.0/1024.0/1024.0, 'f', 9, 64),
-		strconv.FormatFloat(float64(endSize)/1024.0/1024.0/1024.0, 'f', 9, 64),
+		strconv.FormatFloat(float64(startSize)/1024.0/1024.0, 'f', 9, 64),
+		strconv.FormatFloat(float64(endSize)/1024.0/1024.0, 'f', 9, 64),
 	})
 }
 
@@ -54,42 +55,13 @@ func (s *stats) flush() {
 	}
 }
 
-// sizeAggregator collects size per inode, so it's able to size of hardlinked files correctly
-type sizeAggregator struct {
-	files map[uint64]uint64
-}
+func du(path string) (uint64, error) {
+	cmd := exec.Command("du", "-ks", path)
+	output, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+	str := string(output)
 
-func (a *sizeAggregator) sum() uint64 {
-	var sum uint64
-	for _, v := range a.files {
-		sum += v
-	}
-	return sum
-}
-
-func (a *sizeAggregator) collect(path string, info os.FileInfo, err error) error {
-	if info == nil {
-		return errors.New("info is nil")
-	}
-	sys := info.Sys()
-	if sys == nil {
-		return errors.New("Sys() is nil")
-	}
-	stat, ok := info.Sys().(*syscall.Stat_t)
-	if !ok {
-		return errors.New("Not a syscall.Stat_t")
-	}
-
-	if info.IsDir() {
-		a.files[stat.Ino] = uint64(info.Size())
-	}
-	return nil
-}
-
-func getSize(path string) (uint64, error) {
-	a := sizeAggregator{files: make(map[uint64]uint64)}
-	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-		return a.collect(path, info, err)
-	})
-	return a.sum(), err
+	return strconv.ParseUint(str[:strings.IndexFunc(str, unicode.IsSpace)], 10, 64)
 }
